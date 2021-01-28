22422212ea8a6878fc6989820091c6a55ffe8e29
Irakoze Hortance <54577367+Irakoze-Hortance@users.noreply.github.com>
2021-01-28 06:29:10 +0200

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -1,17 +1,19 @@
 const mongoose=require('mongoose')
-const Joi=require('joi')
+const Joi=require('joi');
+
 const Schema=mongoose.Schema
 
-const Product=new Schema({
+const ProductSchema=new Schema({
     proId:{
-        type:Number
+        type:String,
+        required:true
     },
     proName:{
         type:String,
         required:true
     },
     catId:{
-        type:Number,
+        type:String,
         required:true
     },
     quantity:{
@@ -30,30 +32,35 @@ const Product=new Schema({
         type:String,
         required:true
     },
-    productImage:{
+    productImages:{
+        type:Array,
+        required:true
+    },
+    tag:{
         type:String,
         required:true
     },
-    tags:{
-        type:Array,
+    orderDate:{
+        type:Date,
         required:true
     }
 })
 
 function validateProductSchema(Product) {
     const JoiSchema=Joi.object({
-        proId:Joi.number().required(),
+        proId:Joi.string().required(),
         proName:Joi.string().required().max(20).min(2),
-        catId:Joi.number().required(),
+        catId:Joi.string().required(),
         quantity:Joi.number().required(),
         price:Joi.number().required(),
         description:Joi.string().required().max(20).min(2),
         review:Joi.string().required().max(20).min(2),
         tag:Joi.string().required(),
-        productImage:Joi.string().required()
+        orderDate:Joi.date().required(),
+        productImages:Joi.array().required()
     }).options({abortEarly:false});
 return JoiSchema.validate(Product)
    
 }
-module.exports.Product=mongoose.model('Product',Product)
+module.exports.Product=mongoose.model('Product',ProductSchema)
 module.exports.validateProductSchema=validateProductSchema
